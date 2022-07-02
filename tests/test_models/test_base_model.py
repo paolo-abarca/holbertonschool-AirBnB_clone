253f98d16745cc0ec5ea58ff94bdff54b5fb5634
gpradinett <gpradinett@gmail.com>
2022-07-02 15:30:06 -0500

    folder for test files

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import unittes
+from models import storage
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+"""
+Unittest for class Base Model
+"""
+
+
+class TestBase_model(unittest.TestCase):
+    """ comment """
+
+    def prueb(self):
+
+        """
+        este lo hago pa divertirme
+        """
+        pass
