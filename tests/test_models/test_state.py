6c39f7b4c25c55ffde0b5475e981129b5afa7f72
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-07-02 23:27:11 -0500

    updating tests for State

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -25,6 +25,26 @@ class TestState(unittest.TestCase):
         self.assertEqual(my_state.my_number, 22)
         self.assertEqual(type(my_state.__dict__), dict)
 
+    def test_update_state(self):
+        """
+        method 2 that tests State update
+        """
+        my_state_2 = State()
+        my_state_2.name = "California_2"
+        my_state_2.my_number = 50
+        hour_1 = my_state_2.updated_at
+        my_state_2.save()
+        my_state_2_json = my_state_2.to_dict()
+
+        my_state_2.name = "California_2.0"
+        my_state_2.my_number = 55
+        hour_2 = my_state_2.updated_at
+        my_state_2.save()
+        my_state_2_json_2 = my_state_2.to_dict()
+
+        self.assertNotEqual(hour_1, hour_2)
+        self.assertNotEqual(my_state_2_json, my_state_2_json_2)
+
 
 if __name__ == "__main__":
     unittest.main()
