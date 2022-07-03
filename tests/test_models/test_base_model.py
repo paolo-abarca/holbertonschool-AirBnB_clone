3e97b226c730286cec74fbd3de56c46d551c2b06
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-07-02 22:12:30 -0500

    adding tests for BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
+"""
+Unittest for class Base Model
+"""
 import unittest
 from models import storage
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
-"""
-Unittest for class Base Model
-"""
 
 
 class TestBase_model(unittest.TestCase):
-    """ comment """
-
-    def prueb(self):
-
+    """
+    basic tests for BaseModel
+    """
+    def test_base_model(self):
         """
-        este lo hago pa divertirme
+        method that tests BaseModel
         """
-        pass
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+        my_model.save()
+        my_model_json = my_model.to_dict()
+
+        self.assertEqual(type(my_model).__name__, "BaseModel")
+        self.assertEqual(my_model.name, "My First Model")
+        self.assertEqual(my_model.my_number, 89)
+        self.assertEqual(type(my_model.__dict__), dict)
+
+
+if __name__ == "__main__":
+    unittest.main()
