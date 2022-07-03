b3a09eeb402235792e2d7942d6c19072878b796b
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-07-03 01:39:27 -0500

    updating tests for FileStorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,6 +4,8 @@ Unittest for class File Storage
 """
 import unittest
 from models import storage
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 
 class TestFileStorage(unittest.TestCase):
@@ -17,6 +19,20 @@ class TestFileStorage(unittest.TestCase):
         all_objs = storage.all()
 
         self.assertEqual(type(all_objs), dict)
+
+    def test_filestorage_2(self):
+        """
+        method 2 that tests Filestorage
+        """
+        my_filestorage = BaseModel()
+        my_filestorage.name = "Paolo"
+        my_filestorage.save()
+        storage.reload()
+        storage.all()
+        self.assertTrue(storage.all(), "Paolo")
+        self.assertTrue(hasattr(my_filestorage, 'save'))
+        self.assertNotEqual(my_filestorage.created_at,
+                            my_filestorage.updated_at)
 
 
 if __name__ == "__main__":
