9978d88842f29d2a01ec7cd602fc48875df57035
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-07-03 14:03:20 -0500

    adding new tests for FileStorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -15,6 +15,16 @@ from models.review import Review
 import os
 
 
+def setUpModule():
+    """setup module """
+    pass
+
+
+def tearDownModule():
+    """teardown module """
+    pass
+
+
 class TestFileStorage(unittest.TestCase):
     """
     basic tests for FileStorage
@@ -75,6 +85,57 @@ class TestFileStorage(unittest.TestCase):
 
         self.assertEqual(self.new.to_dict()['id'], obj.to_dict()['id'])
         self.assertTrue(os.path.exists('file.json'))
+
+
+class TestModels(unittest.TestCase):
+
+    def setUp(self):
+        """ Set a variable """
+        self.my_model = BaseModel()
+        self.fisto = FileStorage()
+        print("setUp")
+
+    def tearDown(self):
+        """ End variable """
+        print("tearDown")
+
+    @classmethod
+    def setUpClass(cls):
+        """ Set a Class """
+        print("setUpClass")
+
+    @classmethod
+    def tearDownClass(cls):
+        """ Del a Class"""
+        print("tearDownClass")
+
+    def test_file_storage_doc(self):
+        """ Check the documentation """
+        self.assertIsNotNone(FileStorage.__doc__)
+        self.assertIsNotNone(FileStorage.__init__.__doc__)
+        self.assertIsNotNone(FileStorage.all.__doc__)
+        self.assertIsNotNone(FileStorage.new.__doc__)
+        self.assertIsNotNone(FileStorage.save.__doc__)
+        self.assertIsNotNone(FileStorage.reload.__doc__)
+
+    def test_fiel_storage_exist(self):
+        """ Check if methods exists """
+        self.assertTrue(hasattr(self.fisto, "__init__"))
+        self.assertTrue(hasattr(self.fisto, "all"))
+        self.assertTrue(hasattr(self.fisto, "new"))
+        self.assertTrue(hasattr(self.fisto, "save"))
+        self.assertTrue(hasattr(self.fisto, "reload"))
+
+    def test_models_save(self):
+        """ Check if the save function works """
+        self.my_model.name = "Halo"
+        self.my_model.save()
+        storage.reload()
+        storage.all()
+        self.assertTrue(storage.all(), "Halo")
+        self.assertTrue(hasattr(self.my_model, 'save'))
+        self.assertNotEqual(self.my_model.created_at,
+                            self.my_model.updated_at)
 
 
 if __name__ == "__main__":
